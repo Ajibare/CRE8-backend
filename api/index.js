323f12089f55d12b5e23259f2b08562a11e70f50
Ajibare <ajibare.jide@gmail.com>
2026-04-17 14:24:51 +0100

    Fix Vercel serverless handler export

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
 const app = require('../dist/index.js');
-module.exports = app.default || app;
+const expressApp = app.default || app;
+
+// Vercel serverless handler
+module.exports = (req, res) => {
+  return expressApp(req, res);
+};
